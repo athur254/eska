351b052bee29e92ecfb4a4e35393699cf054805b
athur254 <50860508+athur254@users.noreply.github.com>
2019-05-21 09:40:45 +0300

    Create athur.py

---
+++ athur.py
@@ -0,0 +1,13 @@
+class person():
+  def __init__(self,name,age,height):
+      self.name = name
+      self.age = age
+      self.height = height
+      
+  def greetings(name):
+      print ("Hello %s",name)
+      
+  
+athur = person()
+athur.greetings("Patrick")
+      
